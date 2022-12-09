7dc9c40317a87b3de4c1db42ce1626203180fd1c
iJonathan-Dev <ignatius.jonathan@outlook.com>
2022-12-09 15:20:26 +0700

    Render recipe list & update css

--- src/Component/RecipeModal.jsx
+++ src/Component/RecipeModal.jsx
@@ -1,8 +1,10 @@
+import { render } from "@testing-library/react";
 import React, { useState, useEffect } from "react";
 
 const RecipeModal = ({ handleClose, ...props }) => {
   const recipeID = props.recipeId;
   const [recipe, setRecipe] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     fetch("https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=" + recipeID)
@@ -11,15 +13,43 @@ const RecipeModal = ({ handleClose, ...props }) => {
         return response.json();
       })
       .then((data) => {
-        console.table(data.drinks);
-        setRecipe(data.drinks);
+        console.table(data.drinks[0]);
+        setIsLoading(false);
+        setRecipe(data.drinks[0]);
       });
   }, [recipeID]);
 
+  const ingredientsList = () => {
+    let list = [];
+    for (let i = 1; i < 16; i++) {
+      if (recipe[`strIngredient${i}`] !== null) {
+        list.push(
+          <li>
+            {recipe[`strMeasure${i}`]} {recipe[`strIngredient${i}`]}
+          </li>
+        );
+      }
+    }
+    console.log(list);
+    return list;
+  };
+
   return (
     <div className="recipe-modal">
-      {recipe && <h2 className="recipe-title">{recipe[0].strDrink}</h2>}
-      <button onClick={handleClose}>Close</button>
+      {isLoading && <p>Loading...</p>}
+      {recipe && (
+        <>
+          <h2 className="recipe-name">{recipe.strDrink}</h2>
+          <div className="container">
+            <img className="drink-thumb" src={recipe.strDrinkThumb} />
+            <h3 className="container-heading">Ingridients</h3>
+            <ul className="ingridients-list">{ingredientsList()}</ul>
+            <h3 className="container-heading">Instructions</h3>
+            <p className="recipe-instructions">{recipe.strInstructions}</p>
+          </div>
+        </>
+      )}
+      <img className="btn-close" src="https://img.icons8.com/color/48/null/close-window.png" onClick={handleClose} />
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import "./index.scss";
 import CocktailApp from "./Component/CocktailApp";
 import reportWebVitals from "./reportWebVitals";
 
