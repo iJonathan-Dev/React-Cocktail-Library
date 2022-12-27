32b36c32fb110117633447651535ea45ed28fbcf
iJonathan-Dev <ignatius.jonathan@outlook.com>
2022-12-27 14:14:21 +0700

    Add Comment

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -4,12 +4,22 @@ import RecipeModal from "./RecipeModal";
 import "./Home.scss";
 
 const Home = () => {
+  //input search state
   const [searchValue, setSearchValue] = useState("");
+
+  //cocktail list search result state
   const [cocktailList, setCocktailList] = useState(null);
+
+  //Placeholder text for search result state
   const [placeholder, setPlaceholder] = useState(null);
+
+  //Modal box state
   const [viewRecipe, setViewRecipe] = useState(false);
+
+  //Reciped ID state
   const [recipeID, setRecipeID] = useState("");
 
+  //fetch data from API
   const doSearch = (e, searchValue) => {
     e.preventDefault();
     fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + searchValue)
@@ -26,12 +36,14 @@ const Home = () => {
       });
   };
 
+  //open Modal box
   const openRecipeModal = (e, id) => {
     e.preventDefault();
     setViewRecipe(true);
     setRecipeID(id);
   };
 
+  //close Modal box
   const handleClose = () => {
     setViewRecipe(false);
   };
@@ -42,7 +54,7 @@ const Home = () => {
         <form onSubmit={(e) => doSearch(e, searchValue)}>
           <label htmlFor="search-box">Search Cocktail Recipe</label>
           <input type="text" id="search-box" placeholder="Type cocktail name..." value={searchValue} onChange={(e) => setSearchValue(e.target.value)} required />
-          <button id="search-button" type="" submit>
+          <button id="search-button" type="submit">
             Search
           </button>
         </form>
